130c1894c0c1cacaecbb67383054d6fc6cd71a46
kenny <koseleononmen@cyberesi.com>
2020-03-11 07:27:47 -0400

    created error handling class

--- error.c
+++ error.c
@@ -1,5 +1,43 @@
 /* Victoria Subritzky Katz vsubrit1
  * Kenneth Oseleononmen koseleo1 
  * error.c
- * 601.220, Spring 2020
+ * 601.220: Spring 2020
  */
+
+#include "error.h"
+
+int handle_error(Error error)
+{
+   switch (error)
+   {
+		case er_yay:
+			break;
+		case er_missing_filename:
+			printf("Failed to supply input filename or output filename, or both");
+			break;
+		case er_open_input_file_failed:
+			printf("Specified input file could not be opened");
+			break;
+		case er_bad_filename:
+			printf("Specified input file is not a properly-formatted PPM file, or reading input somehow fails");
+			break;
+		case er_bad_operation:
+			printf("No operation name was specified, or operation name specified was invalid");
+			break;
+		case er_insuff_args:
+			printf("	Incorrect number of arguments or kind of arguments specified for the specified operation");
+			break;
+		case er_args_out_of_range:
+			printf("Arguments for the specified operation were out of range for the given input image, or otherwise senseless");
+			break;
+		case er_writing_file_failed:
+			printf("Specified output file could not be opened for writing, or writing output somehow fails");
+			break;
+		case er_other:
+			break;
+		default:
+			printf("unknown error type");
+			break;
+		return error;
+   }
+}
--- error.h
+++ error.h
@@ -3,3 +3,22 @@
  * error.h
  * 601.220, Spring 2020
  */
+
+
+#ifndef ERROR_H_
+#define ERROR_H_
+
+typedef enum _error {
+    er_yay,
+    er_missing_filename,
+    er_open_input_file_failed,
+    er_bad_filename,
+    er_bad_operation,
+    er_insuff_args,
+    er_args_out_of_range,
+    er_writing_file_failed,
+    er_other
+}Error;
+
+int handle_error(Error error);
+#endif //ERROR_H_
