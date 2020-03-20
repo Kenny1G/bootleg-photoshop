27ddc69dd8df98f41b66c6e40b6f1d303f30c562
Kenny <oseleonose@gmail.com>
2020-03-20 07:46:29 -0400

    updated test to log data to logfiles

--- project_test.c
+++ project_test.c
@@ -7,24 +7,42 @@
 #include "project.h"
 #include "ppm_io.h"
 #include "imageManip.h"
+#include <string.h>
+
 
 int main()
 {
-	final_test_read("data/building.ppm");
-	final_test_read("data/trees.ppm");
-	test_copy("data/building.ppm");
-	test_copy("data/trees.ppm");
-	test_exposure("data/trees.ppm","results/trees-exp-negone.ppm",-1);
-	test_exposure("data/trees.ppm", "results/trees-exp-one.ppm",1);
+	FILE *logfile = fopen("test/project_test.log", "w+");
+	if (!logfile) {
+		printf("project_test.c::main() opening logfile failed\n");
+		return 1;
+	}	
+
+	final_test_read("data/building.ppm", "test/building.ppm", logfile);
+	fwrite("reading building.ppm passed\n", 1, strlen("reading building.ppm passed\n"), logfile);
+	final_test_read("data/trees.ppm", "test/trees.ppm",logfile);
+	fprintf(logfile, "reading trees.ppm passed\n");
+
+	test_copy("data/building.ppm", logfile);
+	fprintf(logfile, "copying building.ppm passed\n");
+	test_copy("data/trees.ppm", logfile);
+	fprintf(logfile, "copying trees.ppm passed\n");
+
+	test_exposure("data/trees.ppm", "results/trees-exp-one.ppm",1, "test/trees-exp-one.ppm", logfile);
+	fprintf(logfile, "exposure one passed\n");
+	test_exposure("data/trees.ppm","results/trees-exp-negone.ppm",-1, "test/trees-exp-negone.ppm", logfile);	
+	fprintf(logfile, "exposure negone passed\n");
+
+	fclose(logfile);
 }
 
 
 /* creates a basic image*/
-Image *create_image()
+Image *create_image(FILE *logfile)
 {
 	Image *im = malloc(sizeof(Image));
 	if (!im) {
-		printf("Image allocation failed");
+		fprintf(logfile, "Image allocation failed\n");
 		return 0;
 	}
 	im->rows = 100;
@@ -32,7 +50,7 @@ Image *create_image()
 
 	Pixel *px = malloc(sizeof(Pixel) * im->rows * im->cols);
 	if (!px) {
-		printf("pixel allocation failed");
+		fprintf(logfile, "pixel allocation failed\n");
 		free(im);
 		return 0;
 	}
@@ -56,28 +74,37 @@ Image *create_image()
 
 
 /* checks if two image objects are exactly the same */
-void images_equal(Image *actual, Image *read)
+void images_equal(Image *actual, Image *read, FILE *logfile)
 {
 	assert(actual->rows == read->rows);
-	//printf("%d\n", read->rows); //sanity check
-	//printf("%d\n", read->cols); //sanity check
+	//fprintf(logfile, "%d\n", read->rows); //sanity check
+	//fprintf(logfile, "%d\n", read->cols); //sanity check
 	assert(actual->cols == actual->cols);
 	for (int r = 0; r < actual->rows; ++r) {
 		for (int c = 0; c < actual->cols; ++c) {
-			assert((actual->data[(r * actual->cols) + c].r) == (read->data[((r * actual->cols)) + c].r));
-			assert((actual->data[(r * actual->cols) + c].g) == (read->data[(r * actual->cols) + c].g));
-			assert((actual->data[(r * actual->cols) + c].b) == (read->data[(r * actual->cols) + c].b));
+			unsigned char ar = actual->data[(r * actual->cols) + c].r;
+			unsigned char or = read->data[((r * actual->cols)) + c].r;
+			unsigned char ag = actual->data[(r * actual->cols) + c].g;
+			unsigned char og = read->data[((r * actual->cols)) + c].g;
+			unsigned char ab = actual->data[(r * actual->cols) + c].b;
+			unsigned char ob = read->data[((r * actual->cols)) + c].b;
+			if (ar != or || ag != og || ab != ob) {
+				fprintf(logfile, "r: %d %d\ng: %d %d\nb: %d %d", ar, or, ag, og, ab, ob);
+				printf("Uh oh, check logfile\n");
+				fclose(logfile);
+				exit(1);
+			}
 		}
 	}
 }
 
 
 /* Returns the read Image from filename */
-Image *test_read(const char *filename)
+Image *test_read(const char *filename, FILE *logfile)
 {
 	FILE *test_file = fopen(filename, "rb");
 	if (!test_file) {
-		printf("project_test.c::test_read() open test_file failed");
+		fprintf(logfile, "project_test.c::test_read() open test_file failed\n");
 		return NULL;
 	}
 
@@ -96,28 +123,28 @@ Image *test_read(const char *filename)
 /* Reads og_filename
  * Writes a new file using the Image read from file name
  * comapres the read file and the written file to ensure they're the same */
-void final_test_read(const char *og_filename)
+void final_test_read(const char *og_filename, const char *output_filename, FILE *logfile)
 {
 	// read file from data
-	Image *read_im = test_read(og_filename);
+	Image *read_im = test_read(og_filename, logfile);
 
 	// write file we read to test file
-	FILE *test_file = fopen("test.ppm", "wb");
+	FILE *test_file = fopen(output_filename, "wb");
 	if (!test_file) {
-		printf("project_test.c::main() open test_file failed");
+		fprintf(logfile, "project_test.c::main() open test_file failed\n");
 	}
 	Error *error = er_yay;
 	int iRet = write_ppm(test_file, read_im, error); // write image to a file
 	if (iRet == 0) {
-		printf("project_test.c:final_test_read() write_ppm failed");
+		fprintf(logfile, "project_test.c:final_test_read() write_ppm failed\n");
 	}
 
 	// read file we wrote again
-	//printf("%d pixels written\n", iRet); //sanity check
-	Image *actual_im = test_read("test.ppm");
+	//fprintf(logfile, "%d pixels written\n", iRet); //sanity check
+	Image *actual_im = test_read(output_filename, logfile);
 
 	// make sure read and original are the same
-	images_equal(actual_im, read_im); // check if it's the same
+	images_equal(actual_im, read_im, logfile); // check if it's the same
 
 	free(actual_im->data);
 	free(actual_im);
@@ -129,12 +156,12 @@ void final_test_read(const char *og_filename)
 /* Reads og_filename
  * copies the read Image using copy_ppm() 
  * compares copied image and read image to ensure they're the same */
-void test_copy(const char *og_filename)
+void test_copy(const char *og_filename, FILE *logfile)
 {
-	Image *ppm = test_read(og_filename);
+	Image *ppm = test_read(og_filename, logfile);
 	Error eRet;
 	Image *copied_ppm = copy_ppm(ppm, &eRet);
-	images_equal(ppm, copied_ppm);
+	images_equal(ppm, copied_ppm, logfile);
 	assert(copied_ppm->rows == ppm->rows);
 	assert(copied_ppm->cols == copied_ppm->cols);
 
@@ -148,15 +175,27 @@ void test_copy(const char *og_filename)
 /* The following functions read the correct image from the results folder
  * run's the appropriate manipulation function from imageManip 
  * and then compares the outputs to check if they're the same 
+ * and then writes the manipulated image to ouput_filename
  */
-void test_exposure(const char *og_filename, const char *result_filename, float effect_range)
+void test_exposure(const char *og_filename, const char *result_filename, float effect_range, const char *output_filename, FILE *logfile)
 {
-	Image *result_im = test_read(result_filename);
-	Image *og_im = test_read(og_filename);
+	Image *result_im = test_read(result_filename, logfile);
+	Image *og_im = test_read(og_filename, logfile);
 	Error eRet = er_yay;
 	Image *output = copy_ppm(og_im, &eRet);
 	eRet = exposure(effect_range, og_im, output);
-	images_equal(result_im, output);
+	FILE *test_file = fopen(output_filename, "wb");
+	if (!test_file) {
+		fprintf(logfile, "project_test.c::test_exposure open test_file failed\n");
+	}
+	Error *error = er_yay;
+	int iRet = write_ppm(test_file, output, error); // write image to a file
+	if (iRet == 0) {
+		fprintf(logfile, "project_test.c:test_exposure() write_ppm failed\n");
+	}
+	images_equal(result_im, output, logfile);
+
+	
 	free(output->data);
 	free(output);
 	free(result_im->data);
--- project_test.h
+++ project_test.h
@@ -5,26 +5,26 @@
 
 #include "ppm_io.h"
 /* creates a basic image*/
-Image *create_image();
+Image *create_image(FILE *logfile);
 /* checks if two image objects are exactly the same */
-void images_equal(Image *actual, Image *read);
+void images_equal(Image *actual, Image *read, FILE *logfile);
 /* Returns the Image of filename */
-Image *test_read(const char *filename);
+Image *test_read(const char *filename, FILE *logfile);
 
 /* Reads og_filename
  * Writes a new file using the Image read from file name
  * comapres the read file and the written file to ensure they're the same */
-void final_test_read(const char *og_filename);
+void final_test_read(const char *og_filename, const char *output_filename, FILE *logfile);
 
 /* Reads og_filename
  * copies the read Image using copy_ppm() 
  * compares copied image and read image to ensure they're the same */
-void test_copy(const char *og_filename);
+void test_copy(const char *og_filename, FILE *logfile);
 
 /* The following functions read the correct image from the results folder
  * run's the appropriate manipulation function from imageManip 
  * and then compares the outputs to check if they're the same 
  */
 
-void test_exposure(const char *og_filename, const char *result_filename, float effect_range);
+void test_exposure(const char *og_filename, const char *result_filename, float effect_range, const char *output_filename, FILE *logfile);
 #endif //project_test.h
