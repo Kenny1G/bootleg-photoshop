fe18ad552b17dc640b11111a2012f70f9e148ede
Kenny <oseleonose@gmail.com>
2020-03-20 08:05:02 -0400

    SUGOI exposure works

--- imageManip.c
+++ imageManip.c
@@ -14,12 +14,21 @@
 
 Error exposure(float expos_val, Image *input, Image *output)
 {
+	int new_red;
+	int new_blue;
+	int new_green;
 	for (int r = 0; r < input->rows; ++r) {
 		for (int c = 0; c < input->cols; ++c) {
-			Pixel pix = {(input->data[(r * input->cols) + c].r * pow(2, expos_val)), 
-			(input->data[(r * input->cols) + c].g * pow(2, expos_val)), 
-			(input->data[(r * input->cols) + c].b * pow(2, expos_val))};
-			output->data[(r * input->cols) + c] =  pix;
+			new_red = (input->data[(r * input->cols) + c].r * pow(2, expos_val));
+			if (new_red > 255) new_red = 255;
+
+ 			new_blue = (input->data[(r * input->cols) + c].g * pow(2.0f, expos_val));
+			if (new_blue > 255) new_blue = 255;
+
+ 			new_green = (input->data[(r * input->cols) + c].b * pow(2.0f, expos_val));
+			if (new_green > 255) new_green = 255;
+			Pixel pix = {new_red, new_blue, new_green};
+			output->data[(r * input->cols) + c] = pix;
 		}
 	}
 	return er_yay;
