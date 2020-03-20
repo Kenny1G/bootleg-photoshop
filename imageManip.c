01fc136c6e87ed5a325ac8c579e430c33e7c0a4f
Kenny <oseleonose@gmail.com>
2020-03-20 07:45:41 -0400

    exposure code

--- imageManip.c
+++ imageManip.c
@@ -9,9 +9,18 @@
 #include "error.h"
 #include "ppm_io.h"
 #include <stdio.h>
+#include <math.h>
 
 
 Error exposure(float expos_val, Image *input, Image *output)
 {
+	for (int r = 0; r < input->rows; ++r) {
+		for (int c = 0; c < input->cols; ++c) {
+			Pixel pix = {(input->data[(r * input->cols) + c].r * pow(2, expos_val)), 
+			(input->data[(r * input->cols) + c].g * pow(2, expos_val)), 
+			(input->data[(r * input->cols) + c].b * pow(2, expos_val))};
+			output->data[(r * input->cols) + c] =  pix;
+		}
+	}
 	return er_yay;
 }
