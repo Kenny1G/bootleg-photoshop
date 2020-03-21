e0e1df91260a7cbf28045a3696f1770d06d7f77c
Kenny <oseleonose@gmail.com>
2020-03-21 18:54:39 -0400

    it's weird-ish but blend works

--- imageManip.c
+++ imageManip.c
@@ -8,6 +8,7 @@
 #include "imageManip.h"
 #include "error.h"
 #include "ppm_io.h"
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
@@ -37,6 +38,47 @@ Error exposure(float expos_val, Image *input, Image *output)
 
 Error blend(float alpha, Image *input1, Image *input2, Image *output)
 {
+	Image *image_with_smaller_col = input2;
+	Image *image_with_smaller_row = input2;
+	Image *image_with_bigger_row = input1;
+	Image *image_with_bigger_col = input1;
+	
+	if (input2->cols > input1->cols) {
+		image_with_bigger_col = input2;
+		image_with_smaller_col = input1;
+		output->cols = input2->cols;
+	}
+	
+	if (input2->rows > input1->rows) {
+		image_with_bigger_row = input2;
+		image_with_smaller_row = input1;
+		output->rows = input2->rows;
+	}
+
+	Pixel * tmp = realloc(output->data, sizeof(Pixel) * image_with_bigger_row->rows * image_with_bigger_col->cols);
+	output->data = tmp;
+
+	Pixel black = {0,0,0};
+	for (int r = 0; r < output->rows; ++r) {
+		for (int c = 0; c < output->cols; ++c) {
+			if (r > image_with_smaller_row->rows && c <= image_with_bigger_row->cols) {
+				output->data[(r * output->cols) + c] = image_with_bigger_row->data[(r * image_with_bigger_row->cols) + c];
+			}
+			else if (c > image_with_smaller_col->cols && r <= image_with_bigger_col->rows) {
+				output->data[(r*output->cols) + c] = image_with_bigger_col->data[(r* image_with_bigger_col->cols) + c];
+			}
+			else if (r <= image_with_smaller_row->rows && c <= image_with_smaller_col->cols) {
+				int alphar = (alpha * input1->data[(r * output->cols) + c].r) + ( (1 - alpha) * input2->data[(r * output->cols) + c].r);
+				int alphag = (alpha * input1->data[(r * output->cols) + c].g) + ( (1 - alpha) * input2->data[(r * output->cols) + c].g);
+				int alphab = (alpha * input1->data[(r * output->cols) + c].b) + ( (1 - alpha) * input2->data[(r * output->cols) + c].b);
+				Pixel pix = {alphar, alphag, alphab};
+				output->data[(r*output->cols) + c] = pix;
+			}
+			else {
+				output->data[(r*output->cols) + c] = black;
+			}
+		}
+	}
 	free(input2->data);
 	free(input2);
 	return er_yay;
